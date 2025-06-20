53b177552cc2f70e1f829bcc195f4c97e6a29f8f
RaihanKabir277 <raihank0198@gmail.com>
2025-06-20 22:08:45 +0600

    setup and snake body added here

--- snakeBody.py
+++ snakeBody.py
@@ -2,10 +2,26 @@
 from turtle import Turtle, Screen
 import random
 
-timmy = Turtle()
-
-
 
 screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game ~")
+
+# cordinate_list = [0, -20, -40]
+
+# for i in range(3):
+#     timmy = Turtle("square")
+#     timmy.color("white")
+#     timmy.goto(cordinate_list[i], 0)
+
+# --------- another way for do this ---------
+coordinate_list = [(0, 0), (-20, 0), (-40, 0)]
+for position in coordinate_list:
+    timmy = Turtle("square")
+    timmy.color("white")
+    timmy.goto(position)
+
+
 screen.exitonclick()
 
