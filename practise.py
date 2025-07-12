0bc89e8a1f87c4323754ba977b6ee7568d3ed111
RaihanKabir277 <raihank0198@gmail.com>
2025-07-12 07:29:10 +0600

    slicing addded here for list at the same time tuple also

---
+++ practise.py
@@ -0,0 +1,14 @@
+
+piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
+
+# print(piano_keys[::-1])
+# print(piano_keys[::2])    #it will incremant and only take the second number from each value
+
+#   -------- it also work for tuples at the same time -----
+
+piano_tuples = ("do", "re", "mi", "fa", "so", "la", "ti")
+# print(piano_tuples[2:5])
+# print(piano_tuples[::-1])
+
+
+
